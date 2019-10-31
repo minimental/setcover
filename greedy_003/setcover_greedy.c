685bce7931e35955a9bfb0d3a1fd7421737595c2
Herrmann, Max <m.herrmann@blaetterundsterne.org>
2019-10-31 15:12:47 +0100

    Initial commit
    File `setcover_greedy.c' should eventually cover the code for a greedy approach to the set cover problem.
    Algorithm taken from
    
    "Approximation Algorithms"
    Vazirani, Vijay V.
    2003, 2e

---
+++ greedy_003/setcover_greedy.c
@@ -0,0 +1,12 @@
+int main(int nargs, char** args) {
+	
+	// number of items that need to be covered by the sets
+	int numberOfItems = 9;
+	
+	// initialize an array with the same number of element than there are items
+	int C[numberOfItems];
+	for (int i = 0; i < numberOfItems; ++i)
+		C[i] = -1;
+	
+	return 0;
+}
