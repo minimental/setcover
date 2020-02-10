bde6973e8500284bf40516e39b2c51e647401e3a
Herrmann, Max <m.herrmann@blaetterundsterne.org>
2020-02-10 14:03:56 +0100

    WIP: Implementing test client for performance measures
    Files can now be sorted according to the number of sets they encode.

--- greedy_003/test_client_performance.c
+++ greedy_003/test_client_performance.c
@@ -3,23 +3,55 @@
 #include <stdio.h>
 
 #define LENGTH_OF_FILE_NAME 260
+#define LENGTH_OF_NUMBER_OF_SETS_STRING 8
 
-/*
-	The name of a file is "sc_"<number of sets>"_"<some other integer>
-*/
+int extractNumber(char* string) {
+
+	// find the first occurence of '_' in string
+	char* numberOfSetsStartString = strchr((char*) string, '_') + 1;
+	// find the last occurence of '_' in string
+	char* numberOfSetsEndString = strchr(numberOfSetsStartString, '_') - 1;
+	
+	// file name does not fit convention
+	if (numberOfSetsStartString == 0 || numberOfSetsEndString == 0)
+		return -1;
+	
+	// extract number-of-sets string and convert it into an integer
+	char numberOfSetsString[LENGTH_OF_NUMBER_OF_SETS_STRING];
+	char* numberIndex = numberOfSetsStartString;
+	int i = 0;
+	while (!(numberIndex > numberOfSetsEndString)) {
+		numberOfSetsString[i] = *numberIndex;
+		++numberIndex;
+		++i;
+	}
+	// terminate number string
+	numberOfSetsString[i] = 0;
+
+	return atoi(numberOfSetsString);
+
+}
+
+// compare two file names by extracting number of sets
 int compareTwoFiles(const void* string1, const void* string2) {
 	
-	// find the first occurence of '_' in string 1
-	printf("First string: %s\n", (char*) string1);
-	char* numberOfSetsStartString1 = strchr((char*) string1, '_') + 1;
-	// find the last occurence of '_' in string 1
-	char* numberOfSetsEndString1 = strchr(numberOfSetsStartString1, '_') - 1;
+	// determine number of sets contained in the file name
+	int numberOfSets1, numberOfSets2;
 	
-	printf("Number of sets encoded in string 1: ");
-	char* index = numberOfSetsStartString1;
-	while (index != numberOfSetsEndString1 + 1)
-		printf("%c", *index++);
-	printf("\n");
+	numberOfSets1 = extractNumber((char*) string1);
+	numberOfSets2 = extractNumber((char*) string2);
+	
+	// entries that does not match the file naming convention should be placed at end
+	if (numberOfSets1 < 0)
+		return 1;
+	if (numberOfSets2 < 0)
+		return -1;
+	
+	if (numberOfSets1 > numberOfSets2)
+		return 1;
+	
+	if (numberOfSets1 < numberOfSets2)
+		return -1;
 	
 	return 0;
 }
@@ -29,10 +61,11 @@ void sortFileNamesBySetNumbers(char* fileNames, int numberOfFiles) {
 }
 
 int main() {
-	char fileNames[2 * LENGTH_OF_FILE_NAME];
+	char fileNames[3 * LENGTH_OF_FILE_NAME];
 	
 	char* fileName1String = "sc_105_0";
 	char* fileName2String = "sc_3023_3";
+	char* fileName3String = "sc_1600_1";
 	
 	int fileNameIndex = 0, fileNamesIndex = 0;
 	char character;
@@ -53,7 +86,20 @@ int main() {
 	// terminate string
 	fileNames[fileNamesIndex] = 0;
 	
-	sortFileNamesBySetNumbers(&(fileNames[0]), 2);
+	fileNameIndex = 0; fileNamesIndex = 2 * LENGTH_OF_FILE_NAME;
+	while ((character = fileName3String[fileNameIndex]) != 0) {
+		fileNames[fileNamesIndex] = fileName3String[fileNameIndex];
+		++fileNameIndex;
+		++fileNamesIndex;
+	}
+	// terminate string
+	fileNames[fileNamesIndex] = 0;	
+	
+	sortFileNamesBySetNumbers(&(fileNames[0]), 3);
+	
+	printf("main: 1st file name: %s\n", &(fileNames[0]));
+	printf("main: 2nd file name: %s\n", &(fileNames[LENGTH_OF_FILE_NAME]));
+	printf("main: 3rd file name: %s\n", &(fileNames[2 * LENGTH_OF_FILE_NAME]));
 	
 	return 0;
 }
