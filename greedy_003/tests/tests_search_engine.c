3d0a17a8a62b122f18660ed2bdca54fc67b0a84a
Herrmann, Max <m.herrmann@blaetterundsterne.org>
2020-11-17 23:56:06 +0100

    WIP: Added test suite for search engine

---
+++ greedy_003/tests/tests_search_engine.c
@@ -0,0 +1,11 @@
+#include "tinytest.h"
+
+int search_engine_should_accept_problem_description_and_maximum_weight_as_parameters() {
+	return 0;
+}
+
+TINYTEST_SUITE_START(SEARCH_ENGINE);
+	TINYTEST_ADD_TEST(search_engine_should_accept_problem_description_and_maximum_weight_as_parameters, NULL, NULL);
+TINYTEST_SUITE_END;
+
+TINYTEST_MAIN(SEARCH_ENGINE);
