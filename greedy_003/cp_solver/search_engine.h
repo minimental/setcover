17b2e4a03192b81d4b1dbdc44af0e4059d3e200e
Herrmann, Max <m.herrmann@blaetterundsterne.org>
2020-11-18 22:25:34 +0100

    WIP: Added test setup that greedily solves sc_6_1 a priori

--- greedy_003/cp_solver/search_engine.h
+++ greedy_003/cp_solver/search_engine.h
@@ -1,3 +1,4 @@
-#include "..\problem.h"
+#include "..\greedy_with_LUT\types.h"
+#include "..\greedy_with_LUT\functions.h"
 
 void search_engine(const struct problem *specific_problem, double cost);
--- greedy_003/tests/tests_search_engine.c
+++ greedy_003/tests/tests_search_engine.c
@@ -1,20 +1,40 @@
 /*
  * Compile with:
- * gcc -o tests_search_engine.exe tests_search_engine.c ..\cp_solver\search_engine.c
+ * gcc -o tests_search_engine.exe tests_search_engine.c ..\cp_solver\search_engine.c ..\greedy_with_LUT\read_problem_description_from_file.c ..\greedy_with_LUT\greedy_with_LUT_core.c ..\greedy_with_LUT\dynamic_array_element.c ..\greedy_with_LUT\dynamic_array_index_pair.c ..\greedy_with_LUT\remove_element_from_all_sets.c ..\greedy_with_LUT\find_most_cost_efficient_set.c
  */
 
-#include "..\cp_solver\search_engine.h"
 #include "tinytest.h"
 
+#include "..\cp_solver\search_engine.h"
+
+struct problem specific_problem;
+struct solution specific_solution;
+
+int setup_greedy_solution_6_1() {
+	
+	char *path = "..\\..\\data\\sc_6_1";
+
+	// read problem description from file and create element-value-table
+	read_problem_description_from_file(path, &specific_problem);
+
+	// solve problem
+	greedy_with_LUT_core(&specific_problem, &specific_solution);
+
+}
+
 int search_engine_should_accept_problem_description_and_maximum_cost_as_parameters() {
-	struct problem specific_problem;
 	double cost;
 	search_engine(&specific_problem, cost);
+	return 1;
+}
+
+int search_engine_returns_111110_for_sc_6_1() {
 	return 0;
 }
 
 TINYTEST_START_SUITE(SEARCH_ENGINE);
 	TINYTEST_ADD_TEST(search_engine_should_accept_problem_description_and_maximum_cost_as_parameters, NULL, NULL);
+	TINYTEST_ADD_TEST(search_engine_returns_111110_for_sc_6_1, setup_greedy_solution_6_1, NULL);
 TINYTEST_END_SUITE();
 
 TINYTEST_MAIN_SINGLE_SUITE(SEARCH_ENGINE);
